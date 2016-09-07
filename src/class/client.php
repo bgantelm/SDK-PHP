acd88d8c901df3a7232a2346f98a06beb911cf25
Bruno <bruno.gantelmi@recast.ai>
2016-09-07 15:01:04 +0200

    add request recast

--- src/class/client.php
+++ src/class/client.php
@@ -1,17 +1,23 @@
-<?php
-
+ <?php
 namespace Client;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 
 class Client
 {
-  var $token;
-  var $language;
+  var $token = '4d416c43f41a1fa809db7932cae854c1';
+  var $language = 'en';
+  var $text = 'Quel est la capitale de la France?';
 
-
-  public function textRequest($text, $options)
+// public function Client($token, $language)
+// {
+//   $token = $this->token;
+//   $language = $this->language;
+// }
+  public function textRequest($text)
   {
-    echo 'mdr';
-  //  $params = { text };
+    $params = array('text' => $text);
     if ($this->language)
     {
       echo 'lol';
@@ -25,23 +31,11 @@ class Client
     }
     else
     {
+      $headers = array('Content-Type' => 'application/json', 'Authorization' => "Token " . $this->token);
       $url = 'https://api.recast.ai/v2/request';
 
-      $options = array(
-          'http' => array(
-              'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-              'method'  => 'POST',
-              'content' => $params
-          )
-      );
-      $context  = stream_context_create($options);
-      $result = file_get_contents($url, false, $context);
-      if ($result === FALSE) {
-        print 'error';
-      }
-      var_dump($result);
+      $response = Requests::post($url, $headers, json_encode($params));
+      var_dump($response->body);
     }
   }
-
-$lol = textRequest($text, $options);
 }
